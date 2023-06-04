f8e0b8e43f8fc29e659582ecc7ab6e9d83712d37
kamelmahmud0408 <mahmudulhasankamel1994@gmail.com>
2023-06-04 21:29:55 +0600

    added started template

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+// import AuthProvider from './providers/AuthProvider'
+import { router } from './routes/Routes'
+import { Toaster } from 'react-hot-toast'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+
+  <RouterProvider router={router}>
+    <Toaster></Toaster>
+    </RouterProvider>
+
+)
